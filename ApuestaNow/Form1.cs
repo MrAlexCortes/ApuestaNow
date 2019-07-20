0f8c2359b80af57b733a5732c8be3c596a3d9d35
Alex Cortes <lokitoso_99@hotmail.com>
2019-07-20 16:18:20 -0700

    Prueba 1

--- ApuestaNow/Form1.cs
+++ ApuestaNow/Form1.cs
@@ -16,5 +16,10 @@ namespace ApuestaNow
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola putas");
+        }
     }
 }
